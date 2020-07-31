41a2068ca4b36c437d579322633c7110d8a9c56d
Mario <jorge.fernandez@pragma.com.co>
2020-07-30 19:54:59 -0500

    Updates

---
+++ precache-manifest.3d83b1949e48e490973e88b3a891c35c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c410aaeea4fb0eb4c32e5a3b3521e50d",
+    "url": "/far8_jorge_mario/index.html"
+  },
+  {
+    "revision": "7ca88c08099fc645ecfc",
+    "url": "/far8_jorge_mario/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/far8_jorge_mario/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/far8_jorge_mario/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca88c08099fc645ecfc",
+    "url": "/far8_jorge_mario/static/js/main.92a2f9a7.chunk.js"
+  },
+  {
+    "revision": "01e0b38a5d1d83f689e4",
+    "url": "/far8_jorge_mario/static/js/runtime-main.fcd9827d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/far8_jorge_mario/static/media/logo.5d5d9eef.svg"
+  }
+]);
